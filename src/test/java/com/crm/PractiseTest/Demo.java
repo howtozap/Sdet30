1c4412aa34c23b39b7f5c22cf2fab0d91342f386
rocky <rockysinghsgrl@gmail.com>
2022-04-04 16:57:21 +0530

    hi this is new build

---
+++ src/test/java/com/crm/PractiseTest/Demo.java
@@ -0,0 +1,19 @@
+package com.crm.PractiseTest;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("test2");
+		
+	}
+	
+
+}
